e004766d4d595008fb927bab43e389eb47b5ef0a
Rus <stigmat999@gmail.com>
2017-08-19 17:07:10 +0400

    Initial commit.

---
+++ nginx_log_parser.py
@@ -0,0 +1,21 @@
+﻿nginx_log_file = open("nginx.log")
+
+success_requests = 0
+ip_list = []
+
+for line in nginx_log_file:
+    line_list = line.split(" ")
+
+    if line_list[8] == "500":
+        code500 = open("code500.txt", "a")
+        code500.write(line)
+        code500.close()
+
+    if line_list[8] == "200" and line_list[5] == "\"GET":
+        success_requests += 1
+
+    if line_list[0] not in ip_list:
+        ip_list.append(line_list[0])
+        
+print("200 and GET = " + str(success_requests))
+print("Unique ip: " + str(ip_list))
