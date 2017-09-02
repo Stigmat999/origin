a7dad55c9fae43b78a61c0328356612c58ef7dcb
Rus <stigmat999@gmail.com>
2017-09-02 14:57:14 +0300

    Task 3.

--- nginx_log_parser.py
+++ nginx_log_parser.py
@@ -1,19 +1,36 @@
+import sys
+
 import psycopg2
 
 conn = psycopg2.connect(database="rusbd", user="rus", password="111", host="127.0.0.1", port="5432")
 cur = conn.cursor()
 
-nginx_log_file = open("nginx.log")
+if sys.argv[1] == "fill":
 
-for line in nginx_log_file:
-    line_list = line.split(" ")
+    cur.execute("delete from nginx_log_parser");
 
-    query_params = '{}, \'{}\', \'{}\''.format(int(line_list[8]), line_list[0], line_list[5].replace("\"",""))
+    nginx_log_file = open("nginx.log")
 
-    cur.execute("insert into nginx_log_parser (code, ip, method) \
-        values ({}) ".format(query_params) );
+    for line in nginx_log_file:
+
+        line_list = line.split(" ")
+
+        query_params = '{}, \'{}\', \'{}\''.format(int(line_list[8]), line_list[0], line_list[5].replace("\"",""))
+
+        cur.execute("insert into nginx_log_parser (code, ip, method) \
+            values ({}) ".format(query_params) );
+
+    conn.commit()
+    conn.close()
+
+if sys.argv[1] == "count":
+
+	cur.execute("select count (distinct ip), count (distinct method), count (distinct code) from nginx_log_parser");
+	rows = cur.fetchall()
+
+	for row in rows:
+
+		print("IP: " + str(row[0]) + ", METHOD: " + str(row[1]) + ", CODE: " + str(row[2]))
 
 
-      
-conn.commit()
 conn.close()
