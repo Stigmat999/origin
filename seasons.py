0b4dd140c23c3af0102ba1b204a55001deb76057
Rus <stigmat999@gmail.com>
2017-09-06 15:58:17 +0300

    seasons

---
+++ seasons.py
@@ -0,0 +1,22 @@
+def seasons(a):
+
+    if __name__ == "__main__":
+
+        if 2 < a <= 5:
+            print("Весна")
+
+        elif 5 < a <= 8:
+            print("Лето")
+
+        elif 8 < a <= 11:
+            print("Осень")
+
+        elif a == 12 or a <=2:
+            print("Зима")
+
+    else:
+    	print("Неа =)")
+
+seasons(12)
+
+
