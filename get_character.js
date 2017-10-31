5b46a0d84bffb8e57b0f4f4db8b1cb734322352f
Rus <stigmat999@gmail.com>
2017-10-31 20:56:11 +0100

    marvel first

---
+++ get_character.js
@@ -0,0 +1,37 @@
+function get_name() {
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://gateway.marvel.com:443/v1/public/characters/1011334?ts=1&apikey=204e691d22c22c092ea42a42b12cf6ae&hash=0dd119037baf98be90f0044cec645d1a', false);
+  xhr.send();
+
+  if (xhr.status != 200) {
+    alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+  } 
+    else {
+      var xhr = xhr.responseText;
+      xhr = JSON.parse(xhr);
+      return xhr.data.results[0].name;
+      }
+    
+}
+
+function get_img() {
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://gateway.marvel.com:443/v1/public/characters/1011334?ts=1&apikey=204e691d22c22c092ea42a42b12cf6ae&hash=0dd119037baf98be90f0044cec645d1a', false);
+  xhr.send();
+
+  if (xhr.status != 200) {
+    alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+  } 
+    else {
+      var xhr = xhr.responseText;
+      xhr = JSON.parse(xhr);
+      var img = [xhr.data.results[0].thumbnail.path + "." + xhr.data.results[0].thumbnail.extension];
+
+      return img;
+      }
+    
+}
